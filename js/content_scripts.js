3c4ae58bbf4f66a11aa3f504c7bbf65a6bfaddd6
sarashin <sarashina.nikki65@gmail.com>
2024-10-02 23:43:29 +0900

    fix for UI update

--- js/content_scripts.js
+++ js/content_scripts.js
@@ -3,17 +3,6 @@ var novelId;
 
 window.onload = function () {
     setReadLaterButton();
-    var elem = document.getElementById('novel_honbun');
-    if (elem) {
-        elem.removeAttribute('id');
-        elem.classList.add('novel_honbun');
-    }
-
-    var novelNo = document.getElementById('novel_no');
-    if (novelNo) {
-        novelNo.removeAttribute('id');
-        novelNo.classList.add('novel_no');
-    }
 
     chrome.storage.local.get(function (items) {
         console.log(items);
@@ -26,16 +15,6 @@ window.onload = function () {
         novelId = urlElements[3];
         console.log(novelId);
     }
-
-    var guides = document.getElementsByClassName('novel_bn');
-    var lastGuide = guides[guides.length - 1];
-    if (lastGuide) {
-        lastGuide.innerHTML = null;
-    }
-    var atogaki = document.getElementById('novel_a');
-    if (atogaki) {
-        atogaki.innerHTML = null;
-    }
 };
 
 // 後で読む機能
@@ -47,21 +26,23 @@ function setReadLaterButton() {
     var novelId = '';
     novelId = urlElements[3];
     const url = `https://ncode.syosetu.com/${novelId}/`;
-    var contents1 = document.getElementsByClassName('contents1')[0];
-    var headNav = document.getElementById('head_nav');
+    const announce = document.getElementsByClassName('c-announce')[0];
+    const menu = document.getElementsByClassName('c-menu__body')[0];
 
     var title = '';
     if (urlElements.length === 5) {
-        title = document.getElementsByClassName('novel_title')[0].textContent;
+        title = document.getElementsByClassName('p-novel__title')[0].textContent;
     }
     else if (urlElements.length === 6) {
-        title = contents1.getElementsByTagName('a')[0].textContent;
+        title = announce.getElementsByTagName('a')[0].textContent;
     }
 
-    var laterButton = document.createElement('a');
+    const laterButton = document.createElement('div');
     laterButton.id = 'later-button';
-    laterButton.href = '#';
-    laterButton.innerHTML = '「後で読む」に追加';
+    const span = document.createElement('span');
+    span.innerHTML = '後で読む';
+    laterButton.appendChild(span);
+
     laterButton.addEventListener('click', function() {
         var entity = {};
         const date = new Date();
@@ -80,7 +61,9 @@ function setReadLaterButton() {
             myAlert();
         });
     });
-    headNav.appendChild(laterButton);
+    
+    const adjustLayout = document.getElementsByClassName('p-adjust-layout')[0];
+    adjustLayout.insertAdjacentElement('beforebegin', laterButton);
 }
 
 function myAlert() {
@@ -117,12 +100,12 @@ chrome.runtime.onMessage.addListener(function (req, sender, sendResponse) {
 
 function isExistNext() {
     var ret = false;
-    var guide = document.getElementsByClassName('novel_bn')[0];
-    if (guide) {
-        var guides = guide.getElementsByTagName('a');
-        var guideList = Array.from(guides);
-        guideList.forEach(e => {
-            if (e.textContent[0] == '次') {
+    var pager = document.getElementsByClassName('c-pager')[0];
+    if (pager) {
+        var pages = pager.getElementsByTagName('a');
+        var pageList = Array.from(pages);
+        pageList.forEach(e => {
+            if (e.textContent.startsWith('次')) {
                 ret = true;
             }
         });
@@ -143,48 +126,25 @@ window.document.addEventListener('scroll', function () {
     }
 });
 
-function getNextGuide(content) {
-    var guide = content.getElementsByClassName('novel_bn');
-    var ret = guide[guide.length-1];
-    return ret;
+function getNextPager() {
+    const nextPages = document.getElementsByClassName('c-pager__item--next');
+    const nextPage = nextPages[nextPages.length - 1];
+    return nextPage;
 }
 
-function getCurGuide() {
-    var guide = document.getElementsByClassName('novel_bn');
-    var ret = guide[guide.length - 2];
-    return ret;
+function getPost(content) {
+    const post = content.getElementsByClassName('p-novel')[0];
+    return post;
 }
 
-function getNumber(content) {
-    var novelNo = content.getElementById('novel_no');
-    if (novelNo) {
-        novelNo.removeAttribute('id');
-        novelNo.classList.add('novel_no');
-    }
-    return novelNo;
-}
-
-function getHonbun(content) {
-    var nextHonbun = content.getElementById('novel_honbun');
-    nextHonbun.removeAttribute('id');
-    nextHonbun.classList.add('novel_honbun');
-    return nextHonbun;
-}
-
-function getNextUrl(guide) {
-    var ret = null;
-    var guides = guide.getElementsByTagName('a');
-    var guideList = Array.from(guides);
-    guideList.forEach(e => {
-        if (e.textContent[0] == '次') {
-            ret = e.href;
-        }
-    });
-    return ret;
+function getNextUrl() {
+    const nextPager = getNextPager();
+    const nextUrl = nextPager.href;
+    return nextUrl;
 }
 
 function getNovelTitle() {
-    const title = document.getElementsByClassName('contents1')[0].getElementsByTagName('a')[0];
+    const title = document.getElementsByClassName('c-announce')[0].getElementsByTagName('a')[0];
     const ret = title.textContent;
     return ret;
 }
@@ -193,16 +153,9 @@ document.addEventListener('scrollBottom', function () {
     if (isExistNext()) {
         if (blocked) return;
         blocked = true;
-        var curGuide = getCurGuide();
-        console.log(curGuide);
-        const nextUrl = getNextUrl(curGuide);
+        const nextUrl = getNextUrl();
         console.log(nextUrl);
-        if (nextUrl === null) {
-            const honbuns = document.getElementsByClassName('novel_honbun');
-            var lastHonbun = honbuns[honbuns.length-1];
-            lastHonbun.insertAdjacentElement('afterend', curGuide);
-            return;
-        }
+
         var xhr = new XMLHttpRequest();
         xhr.open('GET', nextUrl, true);
         xhr.responseType = 'document';
@@ -213,59 +166,35 @@ document.addEventListener('scrollBottom', function () {
                 console.log('access failed');
             }
             else {
-                var nextContent = xhr.response;
-                var nextHonbun = getHonbun(nextContent);
-                var nextTitles = nextContent.getElementsByClassName('novel_subtitle');
-                var nextTitle = nextTitles[nextTitles.length-1];
+                const nextContent = xhr.response;
+                const nextPost = getPost(nextContent);
 
-                var honbuns = document.getElementsByClassName('novel_honbun');
-                var lastHonbun = honbuns[honbuns.length-1];
-                console.log(lastHonbun);
-                if (lastHonbun === undefined) {
-                    blocked = false;
-                    return;
+                // 最初のpagerは不要なので削除
+                const nextPager = nextPost.getElementsByClassName('c-pager')[0];
+                nextPost.removeChild(nextPager);
+
+                const posts = document.getElementsByClassName('p-novel');
+                const lastPost = posts[posts.length - 1];
+
+                lastPost.insertAdjacentHTML('afterend', nextPost.outerHTML);
+                nextPost.insertAdjacentHTML('beforebegin', '<p> <br><br> </p>');
+
+                var entity = {};
+                const novelTitle = getNovelTitle(nextContent);
+                const number = nextUrl.split('/')[4];
+                const date = new Date();
+                entity[novelId] = {
+                    "title": novelTitle,
+                    "url": nextUrl,
+                    "number": number,
+                    "date": date.getTime()
                 }
-
-                var parent = document.getElementById('novel_color');
-                var nextGuide = getNextGuide(nextContent);
-
-                var titles = document.getElementsByClassName('novel_subtitle');
-                const titleLists = Array.from(titles);
-
-                var nextNovelNo = getNumber(nextContent);
-
-                console.log(titleLists.length);
-                console.log(titleLists[titleLists.length-1]);
-                if (!titleLists[titleLists.length-1].isEqualNode(nextTitle)) {
-                    console.log(nextTitle);
-                    parent.insertBefore(nextTitle, lastHonbun.nextElementSibling);
-                    titles = document.getElementsByClassName('novel_subtitle');
-                    var lastTitle = titles[titles.length-1];
-                    parent.insertBefore(nextHonbun, lastTitle.nextElementSibling);
-                    parent.insertBefore(nextGuide, lastTitle);
-                    parent.insertBefore(nextNovelNo, lastTitle.nextElementSibling);
-                    
-                    nextGuide.insertAdjacentHTML('beforebegin', '<p> <br><br> </p>');
-                    nextHonbun.insertAdjacentHTML('beforebegin', '<p> <br><br> </p>');
-                    nextTitle.insertAdjacentHTML('beforebegin', '<p> <br><br> </p>');
-
-                    var entity = {};
-                    const novelTitle = getNovelTitle(nextContent);
-                    const number = nextNovelNo.textContent.split('/')[0];
-                    const date = new Date();
-                    entity[novelId] = {
-                        "title": novelTitle,
-                        "url": nextUrl,
-                        "number": number,
-                        "date": date.getTime()
-                    }
-                    console.log(entity);
-                    chrome.storage.local.set(entity, function () {
-                        console.log('bookmark updated!');
-                        chrome.runtime.sendMessage({message: "update", novelId: novelId}, function (res) {
-                        });
+                console.log(entity);
+                chrome.storage.local.set(entity, function () {
+                    console.log('bookmark updated!');
+                    chrome.runtime.sendMessage({message: "update", novelId: novelId}, function (res) {
                     });
-                }
+                });
             }
             blocked = false;
         }
